55df5f734b8d9ba71ea97e75551ae3d10fc0d30a
Anuradha Barnvale <anu.barnwal24@gmail.com>
2020-10-28 20:38:20 +0530

    TTimeirm

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
     res.send('Hello this is a demo app')
 })
 
-var task = cron.schedule('0 34 20 * * *', () => {
+var task = cron.schedule('40 20 * * *', () => {
     console.log("Cron job running")
     db.getUsers()
 });
